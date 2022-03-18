957e7f58b904460363c38eeea6ee5304c052169e
Ricardo Ruiz <ruizmunricardo@gmail.com>
2022-03-18 14:44:43 +0100

    Project KickOff íº

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -4,7 +4,7 @@
            <nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
                <div class="flex items-center flex-shrink-0 text-white mr-6">
                    <px-icon class="mr-4" />
-                   <router-link class="font-semibold text-xl tracking-tight" :to="{ name: 'home'}">Platzi Exchange</router-link>
+                   <router-link class="font-semibold text-xl tracking-tight" :to="{ name: 'home'}">Afro Exchange</router-link>
                </div>
                <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                    <div class="text-sm flex lg:flex-grow justify-start">
