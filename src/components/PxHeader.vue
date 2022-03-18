0248796f8360c2866840be2dbf0d7b246f5ec464
Ricardo Ruiz <ruizmunricardo@gmail.com>
2022-03-18 03:17:56 +0100

    Implementando la funcion cambio dentro de CoinDetail y links en header

--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -6,8 +6,17 @@
                    <px-icon class="mr-4" />
                    <router-link class="font-semibold text-xl tracking-tight" :to="{ name: 'home'}">Platzi Exchange</router-link>
                </div>
-               <div class="hidden sm:block w-full blok flex-grow lg:flex lg:items-center lg:wauto">
-                   <div class="text-sm lg:flex-grow"></div>
+               <div class="hidden sm:block w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+                   <div class="text-sm flex lg:flex-grow justify-start">
+                       <router-link 
+                        v-for="l in links"
+                        :key="l.title"
+                        :to="l.to"
+                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4" 
+                        >
+                        {{ l.title }}
+                        </router-link>
+                   </div>
                </div>
            </nav>
        </nav>
@@ -22,6 +31,13 @@ export default {
     name: 'PxHeader',
     components:{
         PxIcon,
+    },
+
+    props: {
+       links: {
+           type: Array,
+           default: () => [],
+       } 
     }
 }
 </script>
